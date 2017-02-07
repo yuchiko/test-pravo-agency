f4d2700336dc07734b1a9c5122d31d71a71e6409
yuchiko <iyuchiko@gmail.com>
2017-02-07 05:17:32 +0200

    add play buttons

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,40 +1,42 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
 	$('.js_select').selectric();
 
-	$(document).on('click', '.js_service__info', function (e){
+	$(document).on('click', '.js_service__info', function (e) {
 		serviceTooltip.call(e.currentTarget);
 	});
 
-	$(document).on('click', '.burger', function(){
+	$(document).on('click', '.burger', function () {
 		mobileNav.toggle();
 	});
 
+
 });
 
-jQuery(window).load(function() {
+jQuery(window).load(function () {
 	scrollToAnimation();
 	mobileNav.init('.header__menu');
+	modalVideo.init();
 });
 
 function serviceTooltip() {
 	var $thisEl = $(this);
 
 	var block = {
-		offsetX:		$thisEl.offset().left,
-		width:			$thisEl.outerWidth(),
-		parentWidth:	$thisEl.parent().width(),
+		offsetX: $thisEl.offset().left,
+		width: $thisEl.outerWidth(),
+		parentWidth: $thisEl.parent().width(),
 		siblingOffsetX: $thisEl.siblings().offset().left,
-		tooltip:		{
-			el:			$thisEl.find('.service__info'),
-			isShow:		!!(+$thisEl.find('.service__info').css('opacity'))
+		tooltip: {
+			el: $thisEl.find('.service__info'),
+			isShow: !!(+$thisEl.find('.service__info').css('opacity'))
 		}
 	};
 
 	hideAllTooltips();
 
 	block.position = (block.offsetX > block.parentWidth / 2) ? 'left' : 'right';
-	block.tooltip.el.css('width' , block.parentWidth / 2 );
+	block.tooltip.el.css('width', block.parentWidth / 2);
 	if (block.position == 'right') {
 		block.tooltip.el.css(block.position, -block.parentWidth / 2);
 	} else {
@@ -42,8 +44,6 @@ function serviceTooltip() {
 	}
 
 	triggerTooltip();
-
-
 
 
 	function triggerTooltip() {
@@ -85,7 +85,9 @@ function serviceTooltip() {
 
 function scrollToAnimation() {
 	$(document).on('click', 'a[href*="#"]:not([href="#"])', function () {
-		if ($(this).parents('.mobile-nav').length) { mobileNav.close(); }
+		if ($(this).parents('.mobile-nav').length) {
+			mobileNav.close();
+		}
 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -100,7 +102,7 @@ function scrollToAnimation() {
 }
 
 var mobileNav = {
-	mobileClassName: '.mobile-nav',
+	className: '.mobile-nav',
 	mobileMenuClassName: '.mobile-nav__menu',
 	activeClass: 'open',
 	init: function (mainMenuClassName) {
@@ -108,13 +110,37 @@ var mobileNav = {
 			$(mainMenuClassName).children().clone().prependTo(this.mobileMenuClassName);
 		}
 	},
-	open: function() {
-		$(this.mobileClassName).addClass(this.activeClass);
+	open: function () {
+		$(this.className).addClass(this.activeClass);
 	},
-	close: function() {
-		$(this.mobileClassName).removeClass(this.activeClass);
+	close: function () {
+		$(this.className).removeClass(this.activeClass);
 	},
-	toggle: function() {
-		$(this.mobileClassName).hasClass(this.activeClass) ? this.close() : this.open();
+	toggle: function () {
+		$(this.className).hasClass(this.activeClass) ? this.close() : this.open();
+	}
+};
+
+var modalVideo = {
+	$self: $(this),
+	className: '.modal__video',
+	linkClassName: '.modal__link',
+	modalBtn: '.modal__close',
+	activeClass: 'open',
+	init: function () {
+		$(document).on('click', this.linkClassName, function () {modalVideo.open($(this).data('url'));});
+		$(document).on('click', this.modalBtn, function () {modalVideo.close();});
+	},
+	open: function (data_url) {
+		$(this.className).addClass(this.activeClass);
+		data_url = data_url.replace("watch?v=", "v/");
+		$(this.className).find('iframe').prop('src', data_url);
+	},
+	close: function () {
+		$(this.className).removeClass(this.activeClass);
+		$(this.className).find('iframe').prop('src', '');
+	},
+	toggle: function () {
+		$(this.className).hasClass(this.activeClass) ? this.close() : this.open();
 	}
 };
